467d209542f6832261f5523be41ad801e6b7d862
Vosram <63881577+vosram@users.noreply.github.com>
2026-03-18 11:05:37 -0500

    Implement thumbnail uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +32,56 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := fileHeader.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't process file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video from DB", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized", errors.New("You are not authorized"))
+		return
+	}
+	videoThumbnails[video.ID] = thumbnail{
+		data:      fileData,
+		mediaType: mediaType,
+	}
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	newVidMetadata := database.Video{
+		ID: video.ID,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       video.Title,
+			Description: video.Description,
+			UserID:      video.UserID,
+		},
+		UpdatedAt:    time.Now(),
+		ThumbnailURL: &thumbnailUrl,
+	}
+	err = cfg.db.UpdateVideo(newVidMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newVidMetadata)
 }
